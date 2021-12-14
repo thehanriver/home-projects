f25d06389d0a3ff39e528f1fe8b3244de2503ea1
marioisgay123 <hanmario@bu.edu>
2021-12-14 15:52:06 -0800

    Update queue.cpp

---
+++ C++/DSA/queue.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#define SIZE 5
+
+class Queue
+{
+private:
+    int front;
+    int rear;
+    int queue[SIZE];
+
+public:
+    Queue()
+    {
+    }
+};
+
+int main()
+{
+    //peekfront, empty, full, enqueue, dequeue
+}
